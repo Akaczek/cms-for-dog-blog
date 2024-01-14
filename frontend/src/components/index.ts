8510930641d65b552004360510f859474bf4bff8
Akaczek <luk12349876@o2.pl>
2024-01-14 21:32:08 +0100

    Links component

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -4,4 +4,5 @@ export { default as Form } from './Form';
 export { default as Gallery } from './Gallery';
 export { default as Header } from './Header';
 export { default as HeroBanner } from './HeroBanner';
+export { default as Links } from './Links';
 export { default as Text } from './Text';
